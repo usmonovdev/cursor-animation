75b9d8c1ade3f803b9c31a46fabb265bba26ac90
Usmonov Azizbek <usmonovazizbek045@gmail.com>
2023-07-16 15:49:08 +0500

    Premium cursor removed

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,12 +1,10 @@
 import React from "react";
 import ShapeCursors from "./demos/ShapeCursors";
 import WithIcons from "./demos/WithIcons";
-import PremiumCursors from "./demos/PremiumCursors";
 
 const Demo = () => {
   return (
     <div style={{ marginBottom: "40px" }}>
-      <PremiumCursors />
       <ShapeCursors />
       <WithIcons />
     </div>
